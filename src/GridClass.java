3f636ee3e17266e067651dc0969ab6cc1be6e0f4
Elise <50882635+elisecho@users.noreply.github.com>
2020-06-17 22:09:35 -0400

    Add files via upload

---
+++ src/GridClass.java
@@ -0,0 +1,9 @@
+public class GridClass {
+    public static void main(String[]args){
+        int[][] grid;
+
+        {
+            grid = new int[][]{ {1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16} };
+        }
+    }
+}
